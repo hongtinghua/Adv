aaf946572e1448dbeccf24c88ac1203f06d30c10
hongtinghua <1173724387@qq.com>
2018-05-16 09:27:50 +0800

    1222

---
+++ src/com/longs/hong/Test.java
@@ -0,0 +1,16 @@
+package com.longs.hong;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+		Long L1 = 10000000000000000l;
+		Long L2 = null;
+		long l1 = L1;
+		long l2 = L2;
+		}
+
+	}
+
+}
